0fc0d1e0a905e1b23abe299328a3be014cc884c3
Çağatay <cagatayldzz@gmail.com>
2018-12-15 19:40:05 +0000

    update

--- locale/config.js
+++ locale/config.js
@@ -1,66 +1,66 @@
 // Custom Indonesian Moment.js Locales
 // Based on: https://github.com/moment/moment/blob/develop/locale/id.js
 
-moment.locale('id', {
-  months : 'Januari_Februari_Maret_April_Mei_Juni_Juli_Agustus_September_Oktober_November_Desember'.split('_'),
-  monthsShort : 'Jan_Feb_Mar_Apr_Mei_Jun_Jul_Ags_Sep_Okt_Nov_Des'.split('_'),
-  weekdays : 'Minggu_Senin_Selasa_Rabu_Kamis_Jumat_Sabtu'.split('_'),
-  weekdaysShort : 'Min_Sen_Sel_Rab_Kam_Jum_Sab'.split('_'),
-  weekdaysMin : 'Mg_Sn_Sl_Rb_Km_Jm_Sb'.split('_'),
+moment.locale('tr', {
+  months : 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
+  monthsShort : 'Ock_Şub_Mar_Nsn_May_Haz_Tem_Ağu_Eyl_Ekm_Kas_Ara'.split('_'),
+  weekdays : 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
+  weekdaysShort : 'Pzr_Pzrt_Sal_Çar_Per_Cum_Cts'.split('_'),
+  weekdaysMin : 'Pz_Pt_Sl_Çr_Pr_Cu_Ct'.split('_'),
   longDateFormat : {
     LT : 'HH.mm',
     LTS : 'HH.mm.ss',
     L : 'DD/MM/YYYY',
     LL : 'D MMMM YYYY',
-    LLL : 'D MMMM YYYY [pukul] HH.mm',
-    LLLL : 'dddd, D MMMM YYYY [pukul] HH.mm'
+    LLL : 'D MMMM YYYY [saat] HH.mm',
+    LLLL : 'dddd, D MMMM YYYY [saat] HH.mm'
   },
-  meridiemParse: /pagi|siang|sore|malam/,
+  meridiemParse: /sabah|öğlen|akşam|gece/,
   meridiemHour : function (hour, meridiem) {
     if (hour === 12) {
       hour = 0;
     }
-    if (meridiem === 'pagi') {
+    if (meridiem === 'sabah') {
       return hour;
-    } else if (meridiem === 'siang') {
+    } else if (meridiem === 'öğlen') {
       return hour >= 11 ? hour : hour + 12;
-    } else if (meridiem === 'sore' || meridiem === 'malam') {
+    } else if (meridiem === 'akşam' || meridiem === 'gece') {
       return hour + 12;
     }
   },
   meridiem : function (hours, minutes, isLower) {
     if (hours < 11) {
-      return 'pagi';
+      return 'sabah';
     } else if (hours < 15) {
-      return 'siang';
+      return 'öğlen';
     } else if (hours < 19) {
-      return 'sore';
+      return 'akşam';
     } else {
-      return 'malam';
+      return 'gece';
     }
   },
   calendar : {
-    sameDay : '[Hari ini pukul] LT',
-    nextDay : '[Besok pukul] LT',
-    nextWeek : 'dddd [pukul] LT',
-    lastDay : '[Kemarin pukul] LT',
-    lastWeek : 'dddd [lalu pukul] LT',
+    sameDay : '[Bugün] LT',
+    nextDay : '[Yarın] LT',
+    nextWeek : 'dddd [saat] LT',
+    lastDay : '[Dün] LT',
+    lastWeek : 'dddd [sonra] LT',
     sameElse : 'L'
   },
   relativeTime : {
-    future : 'dalam %s',
-    past : '%s yang lalu',
-    s : 'beberapa detik',
-    m : 'semenit',
-    mm : '%d menit',
-    h : 'sejam',
-    hh : '%d jam',
-    d : 'sehari',
-    dd : '%d hari',
-    M : 'sebulan',
-    MM : '%d bulan',
-    y : 'setahun',
-    yy : '%d tahun'
+    future : '%s içinde',
+    past : '%s önce',
+    s : 'birkaç saniye',
+    m : 'dakika',
+    mm : '%d dakika',
+    h : 'saat',
+    hh : '%d saat',
+    d : 'gün',
+    dd : '%d gün',
+    M : 'ay',
+    MM : '%d ay',
+    y : 'yıl',
+    yy : '%d yıl'
   },
   week : {
     dow : 1, // Monday is the first day of the week.
