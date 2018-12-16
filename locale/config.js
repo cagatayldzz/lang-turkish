c7b794bc19056ffe10766450eeaf1c2b111913fc
Çağatay <cagatayldzz@gmail.com>
2018-12-16 21:48:35 +0300

    Update config.js

--- locale/config.js
+++ locale/config.js
@@ -1,69 +1,94 @@
-// Custom Indonesian Moment.js Locales
-// Based on: https://github.com/moment/moment/blob/develop/locale/id.js
+// https://github.com/moment/moment/blob/develop/locale/tr.js
 
-moment.locale('tr', {
-  months : 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
-  monthsShort : 'Ock_Şub_Mar_Nsn_May_Haz_Tem_Ağu_Eyl_Ekm_Kas_Ara'.split('_'),
-  weekdays : 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
-  weekdaysShort : 'Pzr_Pzrt_Sal_Çar_Per_Cum_Cts'.split('_'),
-  weekdaysMin : 'Pz_Pt_Sl_Çr_Pr_Cu_Ct'.split('_'),
-  longDateFormat : {
-    LT : 'HH.mm',
-    LTS : 'HH.mm.ss',
-    L : 'DD/MM/YYYY',
-    LL : 'D MMMM YYYY',
-    LLL : 'D MMMM YYYY [saat] HH.mm',
-    LLLL : 'dddd, D MMMM YYYY [saat] HH.mm'
-  },
-  meridiemParse: /sabah|öğlen|akşam|gece/,
-  meridiemHour : function (hour, meridiem) {
-    if (hour === 12) {
-      hour = 0;
-    }
-    if (meridiem === 'sabah') {
-      return hour;
-    } else if (meridiem === 'öğlen') {
-      return hour >= 11 ? hour : hour + 12;
-    } else if (meridiem === 'akşam' || meridiem === 'gece') {
-      return hour + 12;
-    }
-  },
-  meridiem : function (hours, minutes, isLower) {
-    if (hours < 11) {
-      return 'sabah';
-    } else if (hours < 15) {
-      return 'öğlen';
-    } else if (hours < 19) {
-      return 'akşam';
-    } else {
-      return 'gece';
-    }
-  },
-  calendar : {
-    sameDay : '[Bugün] LT',
-    nextDay : '[Yarın] LT',
-    nextWeek : 'dddd [saat] LT',
-    lastDay : '[Dün] LT',
-    lastWeek : 'dddd [sonra] LT',
-    sameElse : 'L'
-  },
-  relativeTime : {
-    future : '%s içinde',
-    past : '%s önce',
-    s : 'birkaç saniye',
-    m : 'dakika',
-    mm : '%d dakika',
-    h : 'saat',
-    hh : '%d saat',
-    d : 'gün',
-    dd : '%d gün',
-    M : 'ay',
-    MM : '%d ay',
-    y : 'yıl',
-    yy : '%d yıl'
-  },
-  week : {
-    dow : 1, // Monday is the first day of the week.
-    doy : 7  // The week that contains Jan 1st is the first week of the year.
-  }
-});
+;(function (global, factory) {
+   typeof exports === 'object' && typeof module !== 'undefined'
+       && typeof require === 'function' ? factory(require('../moment')) :
+   typeof define === 'function' && define.amd ? define(['../moment'], factory) :
+   factory(global.moment)
+}(this, (function (moment) { 'use strict';
+
+    var suffixes = {
+        1: '\'inci',
+        5: '\'inci',
+        8: '\'inci',
+        70: '\'inci',
+        80: '\'inci',
+        2: '\'nci',
+        7: '\'nci',
+        20: '\'nci',
+        50: '\'nci',
+        3: '\'üncü',
+        4: '\'üncü',
+        100: '\'üncü',
+        6: '\'ncı',
+        9: '\'uncu',
+        10: '\'uncu',
+        30: '\'uncu',
+        60: '\'ıncı',
+        90: '\'ıncı'
+    };
+
+    var tr = moment.defineLocale('tr', {
+        months : 'Ocak_Şubat_Mart_Nisan_Mayıs_Haziran_Temmuz_Ağustos_Eylül_Ekim_Kasım_Aralık'.split('_'),
+        monthsShort : 'Oca_Şub_Mar_Nis_May_Haz_Tem_Ağu_Eyl_Eki_Kas_Ara'.split('_'),
+        weekdays : 'Pazar_Pazartesi_Salı_Çarşamba_Perşembe_Cuma_Cumartesi'.split('_'),
+        weekdaysShort : 'Paz_Pts_Sal_Çar_Per_Cum_Cts'.split('_'),
+        weekdaysMin : 'Pz_Pt_Sa_Ça_Pe_Cu_Ct'.split('_'),
+        longDateFormat : {
+            LT : 'HH:mm',
+            LTS : 'HH:mm:ss',
+            L : 'DD.MM.YYYY',
+            LL : 'D MMMM YYYY',
+            LLL : 'D MMMM YYYY HH:mm',
+            LLLL : 'dddd, D MMMM YYYY HH:mm'
+        },
+        calendar : {
+            sameDay : '[bugün saat] LT',
+            nextDay : '[yarın saat] LT',
+            nextWeek : '[gelecek] dddd [saat] LT',
+            lastDay : '[dün] LT',
+            lastWeek : '[geçen] dddd [saat] LT',
+            sameElse : 'L'
+        },
+        relativeTime : {
+            future : '%s sonra',
+            past : '%s önce',
+            s : 'birkaç saniye',
+            ss : '%d saniye',
+            m : 'bir dakika',
+            mm : '%d dakika',
+            h : 'bir saat',
+            hh : '%d saat',
+            d : 'bir gün',
+            dd : '%d gün',
+            M : 'bir ay',
+            MM : '%d ay',
+            y : 'bir yıl',
+            yy : '%d yıl'
+        },
+        ordinal: function (number, period) {
+            switch (period) {
+                case 'd':
+                case 'D':
+                case 'Do':
+                case 'DD':
+                    return number;
+                default:
+                    if (number === 0) {  // special case for zero
+                        return number + '\'ıncı';
+                    }
+                    var a = number % 10,
+                        b = number % 100 - a,
+                        c = number >= 100 ? 100 : null;
+                    return number + (suffixes[a] || suffixes[b] || suffixes[c]);
+            }
+        },
+        week : {
+            dow : 1, // Monday is the first day of the week.
+            doy : 7  // The week that contains Jan 7th is the first week of the year.
+        }
+    });
+
+    return tr;
+
+})));
